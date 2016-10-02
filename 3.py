07eff0333df4456b7a7f48bfd527eb5a41d7a731
Harm <sizaxyd@gmail.com>
2016-10-02 09:00:05 +0500

    matrix spiral filling

--- 3.py
+++ 3.py
@@ -1,15 +1,15 @@
 import numpy as np
-dim = 5
+dim = 7
 mid = dim // 2
 a = np.zeros([dim, dim])
 a[mid, mid] = 1
 # print(a)
 for i in range(mid):
     # square
-    x = (dim - i * 2) ** 2
-    a[i, i] = x
+    x = (dim - i * 2)
+    print('x=',x)
+    a[i, i] = x ** 2
     # mirrored value
     mir = dim - i - 1
-    print('mir=', mir)
-    a[mir, mir] = x - (mir * 2)
+    a[mir, mir] = a[i, i] - 2*(x - 1)
 print("\n", a)
