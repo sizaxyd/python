1bf7a14a379d73c8d7bb8419281abdb4f440d5f7
Harm <sizaxyd@gmail.com>
2016-10-03 10:30:31 +0500

    added array implementation

--- 3.py
+++ 3.py
@@ -1,15 +1,24 @@
 import numpy as np
-dim = 7
+dim = 101
 mid = dim // 2
-a = np.zeros([dim, dim])
-a[mid, mid] = 1
+# a = np.zeros([dim, dim])
+# a[mid, mid] = 1
 # print(a)
+# for i in range(mid):
+#     # square
+#     x = (dim - i * 2)
+#     a[i, i] = x ** 2
+#     # mirrored value
+#     mir = dim - i - 1
+#     a[mir, mir] = a[i, i] - 2*(x - 1)
+# print("\n", a)
+
+b = np.zeros([dim])
+b[mid] = 1
+# print('b=', b)
 for i in range(mid):
-    # square
-    x = (dim - i * 2)
-    print('x=',x)
-    a[i, i] = x ** 2
-    # mirrored value
+    x = dim - i*2
+    b[i] = x ** 2
     mir = dim - i - 1
-    a[mir, mir] = a[i, i] - 2*(x - 1)
-print("\n", a)
+    b[mir] = b[i] - 2*(x-1)
+print("sum_b=", sum(b))
