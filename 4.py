d293eb48474589ea79dfa7bf1456cc5be19db8a8
harm <sizaxyd@gmail.com>
2016-10-09 21:09:01 +0500

    solved #4

--- 4.py
+++ 4.py
@@ -1,8 +1,15 @@
 import urllib.request
 url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
-num = '12345'
+num = '8022'
 
-with urllib.request.urlopen(url + num) as response:
-    html = response.read()
 
-num = bytes.decode(html)[-5:]
+def fetch():
+    global num
+    with urllib.request.urlopen(url + num) as response:
+        html = response.read()
+    num = bytes.decode(html).split()[-1]
+    print(num)
+    return
+
+while True:
+    fetch()
